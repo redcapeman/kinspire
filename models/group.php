ea15b8499057b09ec676166e365284d8a5f892c9
PurJam <james.louie.puro@gmail.com>
2009-03-04 14:19:23 +0300

    Initial Push of fork ffiles

--- models/group.php
+++ models/group.php
@@ -3,10 +3,6 @@ class Group extends AppModel {
 
 	var $name = 'Group';
 	var $actsAs = array('Acl' => array('requester'));
- 
-	function parentNode() {
-	    return null;
-	}
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $hasMany = array(
@@ -15,6 +11,69 @@ class Group extends AppModel {
 								'dependent' => false
 			)
 	);
+
+var $validate = array( 'name' => VALID_NOT_EMPTY );
+	
+  	function afterSave($created) {
+
+  		if ( $created ) {
+  		
+  		    // its a creation
+
+			$id = $this->getLastInsertID();
+
+			$aro = new Aro();
+
+            $aro->updateAll(    array('alias'=>'\'Group:'.$id.'\''),
+                                array(  'Aro.model'=>'Group',
+                                        'Aro.foreign_key'=>$id)
+                                );
+		}
+		else {
+		
+            // its an edit, we have to update the tree
+            $data = $this->read();
+            $parent_id = $data['Group']['parent_id'];
+
+            $aro = new Aro();
+            
+            $aro_record = $aro->findByForeignKey( $this->id );
+            $parent_record = $aro->findByForeignKey( $parent_id );
+                          
+            if ( empty( $aro_record ) ) {
+            
+                // orphaned child
+                $this->Aro->save( array(
+                    'model' => $this->name,
+                    'foreign_key' => $this->id,
+                    'alias' => $this->name.':'.$this->id,
+    			    'parent_id'		=> $parent_record['Aro']['id']
+                ) );
+            }
+            else {
+            
+                // just moving nodes
+                $this->Aro->save( array(
+                    'model' => $this->name,
+                    'foreign_key' => $this->id,
+                    'alias' => $this->name.':'.$this->id,
+    			    'parent_id'		=> $parent_record['Aro']['id'],
+    				'id'			=> $aro_record['Aro']['id']
+    			) );
+            }    			
+        }
+		
+		return true;
+	}
+	
+	function parentNode(){
+    
+        // This should be the alias of the parent $model::$id
+        $data = $this->read();
+    
+        // This needs to be unique    
+        return 'Group:'.$data['Group']['parent_id'];
+    }
 
 }
 ?>
--- models/user.php
+++ models/user.php
@@ -4,23 +4,6 @@ class User extends AppModel {
 	var $name = 'User';
 	var $actsAs = array('Acl' => array('requester'));
 	 
-	function parentNode() {
-	    if (!$this->id && empty($this->data)) {
-	        return null;
-	    }
-	    $data = $this->data;
-	    if (empty($this->data)) {
-	        $data = $this->read();
-	    }
-	    if (!$data['User']['group_id']) {
-	        return null;
-	    }
-		
-		if ($data['User']['group_id']){
-	        return array('Group' => array('id' => $data['User']['group_id']));
-	    }
-	}
-
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $belongsTo = array(
 			'Group' => array('className' => 'Group',
@@ -66,6 +49,61 @@ class User extends AppModel {
 								'dependent' => false
 			)
 	);
+	
+	
+  	function afterSave($created) {
+
+  		if($created) {
+  		
+  		    // its a creation
+
+			$id = $this->getLastInsertID();
+
+			$aro = new Aro();
+
+			$aro->updateAll( array('alias'=>'\'User:'.$id.'\''),
+                                array(  'Aro.model'=>'User',
+                                        'Aro.foreign_key'=>$id)
+			                     );
+		}
+		else {
+		
+            // its an edit, we have to update the tree
+            $data = $this->read();
+            $parent_id = $data['User']['group_id'];
+
+            $aro = new Aro();
+            
+            $aro_record = $aro->findByAlias( $this->name.':'.$this->id );
+            $parent_record = $aro->findByAlias( 'Group:'.$parent_id );
+                          
+            if ( !empty( $aro_record ) ) {
+            
+                $parent_id = '0';
+                
+                if ( !empty( $parent_record ) ) {
+                    $parent_id = $parent_record['Aro']['id'];
+                }
+                
+                // just changing parents
+                $this->Aro->save( array(
+                    'parent_id'		=> $parent_id,
+    				'id'			=> $aro_record['Aro']['id']
+    			) );
+            }    			
+        }
+        		
+		return true;
+	}
+	
+	function parentNode(){
+    
+        // This should be the alias of the parent $model::$id
+        $data = $this->read();
+
+        // This needs to be unique    
+        return 'Group:'.$data['User']['group_id'];
+    }
 
 }
 ?>
